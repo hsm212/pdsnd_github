fb3dd8dc77862744700263049c12c354bfeabbe2
hsm <hsm-212@hotmail.com>
2024-11-02 16:41:12 +0300

    added comments

--- bikeshare.py
+++ bikeshare.py
@@ -1,6 +1,9 @@
+#importing necessary libraries
 import time
 import pandas as pd
 import numpy as np
+
+# defining the data dictionary where keys are city names and values are their corresponding CSV file names
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
@@ -12,7 +15,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s explore some US bikeshare data!!!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = input('Enter city name: ').lower()
     while city not in CITY_DATA.keys():
